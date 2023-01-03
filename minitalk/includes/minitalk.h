36ac4fd1b57f5a5b0bab406e79bcb1065c861807
Diogo Tavares <diogo.martins.tavares@gmail.com>
2023-01-03 10:03:19 +0000

    skeleton of the project

---
+++ minitalk/includes/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 09:36:42 by diogmart          #+#    #+#             */
+/*   Updated: 2023/01/03 10:02:19 by diogmart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#endif
