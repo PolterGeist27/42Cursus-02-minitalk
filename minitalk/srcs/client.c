fe41e3e61c60833143dad76ff9449491e89ff616
Diogo Tavares <diogo.martins.tavares@gmail.com>
2023-01-03 10:44:04 +0000

    Added a TODO + PID and miss input handling

--- minitalk/srcs/client.c
+++ minitalk/srcs/client.c
@@ -6,12 +6,32 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 09:35:14 by diogmart          #+#    #+#             */
-/*   Updated: 2023/01/03 10:02:13 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/01/03 10:42:22 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
+/*
+TODO:
+	- Check if the first arg is a String of numbers
+	- Check if the second arg is a String
+	- Send the second arg to the server
+	- ...
+Bonus:
+	- Receive and handle the signal from the server
+*/
+
 void	handler();
 
-int	main(int argc, char **argv);
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Wrong input! Try \"./client [PID] [Message]\" instead");
+		return (0);
+	}
+	pid = ft_atoi(argv[1]);
+}
--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,10 +6,32 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 09:33:45 by diogo             #+#    #+#             */
-/*   Updated: 2023/01/03 10:02:44 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/01/03 10:32:00 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-int	main(int argc, char **argv);
+/*
+TODO:
+	- Receive the message from the client
+	- Quickly print the message received
+	- Handle multiple clients
+	- ...
+Bonus:
+	- Send a signal back to the client when a message is received
+	- Support unicode characters
+*/
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	pid = getpid();
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_printf("Wrong input! Try \"./server\" instead");
+		return (0);
+	}
+}
